bddd5858468d9ebb9431037fc3f4ffc1b53146ab
Rebecca Simon <Simon.RebeccaL@gmail.com>
2012-07-23 10:01:44 -0400

    .DS_Store files ignored

---
+++ About/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RebeccaSimonCom::Application.config.secret_token = '0b1bffb5d61ce78936479970b523b6224bd5b0b406bb274458397069d261c2a05518d93abda2850020af2e9576a5321089e8bdb5d9d980aeddbcd156143352e5'
